692662ec9127b6112d05644c073383e72d0089f6
IonTulbure <iontulburedev@gmail.com>
2024-06-17 23:25:26 +0300

    conditional display for city url parameter

--- dynamic-city-links/city.php
+++ dynamic-city-links/city.php
@@ -1,5 +1,26 @@
-<?php require __DIR__ . '/inc/functions.inc.php'; ?>
+<?php
+require __DIR__ . '/inc/functions.inc.php';
+
+$city = null;
+
+if (!empty($_GET['city'])) {
+    $city = $_GET['city'];
+}
+
+// var_dump($city);
+
+if (!empty($city)) {
+    // load
+    // prepare the data
+}
+
+?>
 
 <?php require __DIR__ . '/views/header.inc.php'; ?>
+
+<?php if (empty($city)) : ?>
+    <p>The city could not be loaded.</p>
+<?php else : ?>
+<?php endif; ?>
 
 <?php require __DIR__ . '/views/footer.inc.php'; ?>
